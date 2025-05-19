2fac89923ba92bbe12b1d1723d75297aa5239b6b
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2025-05-19 20:18:05 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
+
+export default function Home() {
+  return (
+    <div>
+      <button className="btn btn-primary">click me</button>
+      <h1 className="text-xl">title</h1>
+      <h1 className="text-5xl font-grotesk">title</h1>
+      <HomeIcon className="w-8 text-black-600" />
+    </div>
+  );
+}
